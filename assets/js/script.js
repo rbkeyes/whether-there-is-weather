d3d631f7b05385b424adda48dd012ebf9a74e5df
Reed Keyes <rbkeyes@gmail.com>
2022-12-15 14:04:09 -0800

    added stylesheet linksk + scripts for javascript, bootstrap, + custom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,57 @@
 // api key for openweather
-var apiKey = "41f329d85d1a516c5b46f4e25159f568";
+const APIKey = "1e4594222d984bf5f75b387428c1a416";
 
 // country codes for united states
 var countryCodeUS = "US";
 var countryCodeUSA = "USA";
 var countryCodeNumUS = 840;
 
-var city;
+var city = "berkeley, us-ca, usa";
 var latitude;
 var longitude;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+let savedSearches = [];
+function runSearch() {
+    getCoords();
+}
 
-var query = fetch(queryURL);
+// get coordinates from search
+function getCoords() {
+    let locationURL =  "http://api.openweathermap.org/geo/1.0/direct?q="+ city + "&limit=" + 5 + "&appid=" + APIKey;
+    fetch(locationURL)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    //   console.log("lat = " + data[0].lat);
+    //   console.log("lon = " + data[0].lon);
+      latitude = (data[0].lat);
+      longitude = (data[0].lon);
+      console.log(latitude);
+      console.log(longitude);
+      localStorage.setItem("latitude = ", JSON.stringify(latitude));
+      localStorage.setItem("longitude = ", longitude);
+    getWeather();
+    })
+};
 
-console.log(query.value);
+function getWeather() {
+    // console.log(localStorage);
+    // var storedLat = JSON.parse(localStorage.getItem('latitude'));
+    // var storedLon = JSON.parse(localStorage.getItem('longitude'));
+    // console.log(storedLat);
+    // console.log(storedLon);
+    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
+fetch(queryURL)
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+};
+
+runSearch();
